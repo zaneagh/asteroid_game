50e97a294fa0f4423f5e6afff61be389fb6dbc84
zaneagh <zanea@DESKTOP-7CSEJEG.>
2025-02-26 17:48:30 -0500

    Player display feature added
    Now drawing a white triangle to the screen that represents the player

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     print(f"Pygame Intialization Success Stats...   {pygame.get_init()}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    game_clock = pygame.time.Clock()
+    dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
     
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
+
+        dt = game_clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
